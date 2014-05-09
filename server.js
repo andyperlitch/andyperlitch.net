0eb229762bfaf50b3f107d5dd5999d156dd535b9
Andy Perlitch <andyperlitch@gmail.com>
2014-05-08 20:45:43 -0700

    updated g2c options

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ app.get('/contact', function(req, res) {
 
 app.use(github2cocoon('ludei_zips', {
     username: 'andyperlitch',
-    tmp_dir: __dirname + '/tmp_zips'
+    tmp_dir: __dirname + '/tmp_zips',
+    debug: true
 }));
 
 app.use(express.static(__dirname + '/public'));
